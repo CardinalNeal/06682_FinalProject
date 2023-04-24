2d812da45f48d421cd6e1e5d139c47e2f01b6897
CardinalNeal <cardinalneal@gmail.com>
2023-04-24 19:07:05 +0000

    reformat command util

--- pkg/s23openalex/command_util.py
+++ pkg/s23openalex/command_util.py
@@ -1,14 +1,29 @@
+"""
+command_util.py: A command-line utility to fetch information for a DOI in BibTeX or RIS format
+using OpenAlex Institutions.
+"""
+
+
 import click
 from .works import Works
 
 
-@click.command(help="OpenAlex Institutions")
+@click.command(
+    help="OpenAlex Institutions: Fetch citation information for a DOI in BibTeX or RIS format."
+)
 @click.argument("doi")
-@click.option("-b", "bibtex_flag", is_flag=True, help="Output format: BibTeX")
-@click.option("-r", "ris_flag", is_flag=True, help="Output format: RIS")
+@click.option("-b", "bibtex_flag", is_flag=True, help="Print BibTeX result")
+@click.option("-r", "ris_flag", is_flag=True, help="Print RIS result")
 def main(doi, bibtex_flag, ris_flag):
-    w = Works(doi)
+    """
+    Fetches and prints the citation information for a given DOI in either BibTeX or RIS format.
+
+    :param doi: Digital Object Identifier (DOI) of the work.
+    :param bibtex_flag: Flag to specify print BibTeX result.
+    :param ris_flag: Flag to specify print RIS result.
+    """
+    works = Works(doi)
     if bibtex_flag:
-        print(w.bibtex)
+        print(works.bibtex)
     if ris_flag:
-        print(w.ris)
+        print(works.ris)
