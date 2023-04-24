35e8a77b865a91f3b571d1d95e0a3775b61e5655
CardinalNeal <cardinalneal@gmail.com>
2023-04-24 23:18:12 +0000

    ALL done except the import error, Next will be test import error

--- pkg/s23openalex/test_example.py
+++ pkg/s23openalex/test_example.py
@@ -1,34 +1,40 @@
 """
-This test file test the basic info of one example
+This test file tests the basic info of one example.
 """
-import pytest
 from s23openalex import Works
 
 
-@pytest.fixture
-def work():
-    return Works("https://doi.org/10.1021/acscatal.5b00538")
-
-
-def test_str(work):
+def test_str():
+    """Test the __str__ method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert str(work) == "str"
 
 
-def test_repr(work):
+def test_repr():
+    """Test the __repr__ method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert isinstance(repr(work), str)
 
 
-def test_related_works(work):
+def test_related_works():
+    """Test the related_works method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert isinstance(work.related_works(), list)
 
 
-def test_references(work):
+def test_references():
+    """Test the references method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert isinstance(work.references(), list)
 
 
-def test_citing_works(work):
+def test_citing_works():
+    """Test the citing_works method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert isinstance(work.citing_works(), list)
 
 
-def test_ris(work):
+def test_ris():
+    """Test the ris method of the Works class."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
     assert "ER  -" in work.ris
